09e10187e4b10a13e2c6034095c6d43ca5207093
Amartya Varma <amartya.varma@gmail.com>
2024-11-18 14:27:27 -0800

    f

--- generate.py
+++ generate.py
@@ -12,6 +12,7 @@ from typing import Optional, Tuple, Union
 import torch
 import torch._dynamo.config
 import torch._inductor.config
+from transformers import AutoTokenizer
 
 def device_sync(device):
     if "cuda" in device:
@@ -289,8 +290,8 @@ def main(
     """
     assert checkpoint_path.is_file(), checkpoint_path
 
-    tokenizer_path = checkpoint_path.parent / "tokenizer.model"
-    assert tokenizer_path.is_file(), str(tokenizer_path)
+    # tokenizer_path = checkpoint_path.parent / "tokenizer.model"
+    # assert tokenizer_path.is_file(), str(tokenizer_path)
 
     global print
     from tp import maybe_init_dist
@@ -318,7 +319,7 @@ def main(
     device_sync(device=device) # MKG
     print(f"Time to load model: {time.time() - t0:.02f} seconds")
 
-    tokenizer = get_tokenizer(tokenizer_path, checkpoint_path)
+    tokenizer = AutoTokenizer.from_pretrained("amuvarma/3days-tagged-noreps-caps")
 
     if isinstance(prompt, str):
         encoded = encode_tokens(tokenizer, prompt, bos=True, device=device)
